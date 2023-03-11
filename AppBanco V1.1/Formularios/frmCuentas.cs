6f6f1ebbc4fba23caa71f9af86bb60c96095d59c
AlexTejeda <alex2227@hotmail.com>
2023-03-11 15:20:12 -0600

    Addded logic for being solved in frmCuentas

--- AppBanco V1.1/Formularios/frmCuentas.cs
+++ AppBanco V1.1/Formularios/frmCuentas.cs
@@ -1,4 +1,5 @@
-﻿using BankClassSourcesDLL.Clases;
+﻿using AppBank_V1._1;
+using BankClassSourcesDLL.Clases;
 using CuentaUserControl;
 using System;
 using System.Collections.Generic;
@@ -13,42 +14,109 @@ using System.Windows.Forms;
 namespace AppBanco_V1._1
 {
     public partial class frmCuentas : Form
-    {
-        public frmCuentas()
+    {   
+        ContenedorCuentas listaCuentas;
+        Cliente Cliente;
+        public frmCuentas(Cliente cliente)
         {
             InitializeComponent();
+            this.Cliente = cliente;
+            listaCuentas = new ContenedorCuentas();
+            Lectura();
+            LlenarFlP();
         }
 
         private void BtnAgregarCuenta_Click(object sender, EventArgs e)
         {
-
+            Cuenta NuevaCuenta = new Cuenta()
+            {
+                Nombre = txtNombre.Text,
+                Fecha  = int.Parse(txtCuenta.Text),
+                NoCuenta = int.Parse(txtCuenta.Text),
+                Monto = Convert.ToChar(txtSexo.Text),
+                TipoTransaccion = 
+            };
+            listaCuentas.AddCuenta(NuevaCuenta);
+            flpCuentas.Controls.Add(getControlCuenta(NuevaCuenta));
         }
-        //public CuentaControl GetCuentaControl (Cuenta cuenta)//
-        //{
-        //    CuentaControl cuentaControl = new CuentaControl();
-        //    cuentaControl.Asignar(cuenta);
-        //    if (File.Exists(@"C:\TAP\EXAMEN\Transacciones\" + cuenta.NoCuenta + ".txt") == false)
-        //    {
-        //        saveFileDialog1.FileName = @"C:\TAP\EXAMEN\Transacciones\" + cuenta.NoCuenta + ".txt";
-        //        Escritura escritura = new Escritura(saveFileDialog1.FileName, true);
-        //        Transaccion transaccion1 = new Transaccion();
-        //        transaccion1.Fecha = cuenta.Fecha;
-        //        if (cuenta.TipoTransaccion == true)
-        //        {
-        //            transaccion1.Tipo = "Abono";
-        //        }
-        //        else
-        //        {
-        //            transaccion1.Tipo = "Retiro";
-        //        }
-        //        transaccion1.Monto = cuenta.Monto;
-        //        escritura.Escribir(transaccion1.ToString());
-        //        escritura.Cerrar();
-        //    }
 
-        //    pb.Click += Pb_Click;
+        public void LlenarFlP()
+        {
+            if (!listaClientes.vacio())
+            {
+                foreach (var item in listaClientes.GetClientes())
+                {
+                    Cliente cliente = item as Cliente;
+                    flpClientes.Controls.Add(getControlCliente(cliente));
+                }
+            }
+        }
+        public void Escritura()
+        {
+            if (flpClientes.Controls.Count != 0)
+            {
+                Writer esc = new Writer(@"C:\TAP\EXAMEN-2\Clientes\ClientesBanco.txt", false);
+                foreach (var item in listaClientes.GetClientes())
+                {
+                    //PictureBox pb = item as PictureBox;
+                    //Cuenta cuenta = item as Cuenta;
+                    esc.Write(item.ToString());
+                }
+                esc.Close();
+            }
+        }
+        public void Lectura()
+        {
+            StreamReader lct = new StreamReader(@"C:\TAP\EXAMEN-2\Clientes\ClientesBanco.txt");
+            Reader lectura = new Reader(lct);
+            string[] obtenido = lectura.ReadAll().Split("\n");
+            for (int i = 0; i < obtenido.Length - 1; i++)
+            {
+                string[] columas = obtenido[i].Split(",");
+                Cliente clienteArchivo = new Cliente()
+                {
+                    Nombre = columas[0],
+                    Id = int.Parse(columas[1]),
+                    Edad = int.Parse(columas[2]),
+                    Sexo = Convert.ToChar(columas[3]),
+                };
+                listaClientes.AddCliente(clienteArchivo);
+            }
+            lectura.Close();
+        }
 
-        //    return pb;
-        //}
+        public void ComprobarArchivoCuenta(Cliente cliente)
+        {
+            //comprueba si existe el archivo con el mismo nombre del Id del cliente
+            //este guarda todas sus cuentas cabe recalcar
+            if (File.Exists(@"C:\TAP\EXAMEN-2\Cuentas\" + cliente.Id + ".txt") == false)
+            {
+                //En caso que no exista se hace uso del savefiledialog
+                saveFileDialog1.FileName = @"C:\TAP\EXAMEN-2\Cuentas\" + cliente.Id + ".txt";
+                Writer escritura = new Writer(saveFileDialog1.FileName, true);
+                escritura.Dispose();
+            }
+        }
+        public CuentaControl getControlCuenta(Cuenta cuenta)
+        {
+            ClienteUserControl.ClienteUserControl controlCliente = new ClienteUserControl.ClienteUserControl();
+            controlCliente.Asignar(cliente);
+            controlCliente.BtnMostrarCuentasClick += ControlCliente_BtnMostrarCuentasClick;
+            ComprobarArchivoCuenta(cliente);
+            controlCliente.Tag = cliente;
+            return controlCliente;
+        }
+        private void ControlCliente_BtnMostrarCuentasClick(object? sender, EventArgs e)
+        {
+            ClienteUserControl.ClienteUserControl control = sender as ClienteUserControl.ClienteUserControl;
+            Cliente cliente = control.Tag as Cliente;
+            frmCuentas frmCuentas = new frmCuentas();
+            frmCuentas.ShowDialog();
+        }
+
+        private void FrmClientes_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Escritura();
+        }
     }
 }
