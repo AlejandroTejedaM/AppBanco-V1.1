d808dbd00f43a03de667533fac5eff01995689cd
AlexTejeda <alex2227@hotmail.com>
2023-03-10 20:43:10 -0600

    Class Reader Added

---
+++ BankClassSourcesDLL/Reader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankClassSourcesDLL
+{
+    public class Reader
+    {
+        StreamReader reader;
+
+        public Reader(string rute)
+        {
+            reader = new StreamReader(rute);
+        }
+
+        public string ReadAll(string rute)
+        {
+            return reader.ReadToEnd();
+        }
+
+        public List<string> ReadByLine()
+        {
+            List<string> list = new List<string>();
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                list.Add(line);
+            }
+            return list;
+        }
+
+        public void Close()
+        {
+            reader.Close();
+        }
+    }
+}
