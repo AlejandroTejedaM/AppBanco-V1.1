b6a75b2c49b49bc3511e18ca1e2a10ddf1755294
Alejandro Tejeda Moreno <alex2227@hotmail.com>
2023-03-11 19:30:58 -0600

    Added funcion that allows add Buttons for each transaction in one account

--- AppBanco V1.1/Formularios/frmCuentaTransacciones.cs
+++ AppBanco V1.1/Formularios/frmCuentaTransacciones.cs
@@ -19,5 +19,34 @@ namespace AppBanco_V1._1
             InitializeComponent();
             this.contenedor = contenedor;
         }
+        public void llenarFLP()
+        {
+            if (contenedor != null)
+            {
+                foreach (var item in contenedor.GetTransacciones())
+                {
+                    flpTransacciones.Controls.Add(getButton(item));
+                }
+            }
+        }
+        public Button getButton(Transaccion a)
+        {
+            Button button = new Button();
+            decimal var = a.Monto;
+            button.Text = var.ToString();
+            if (a.Tipo.Replace(" ", "") == "Abono")//abono
+            {
+                button.BackColor = Color.Green;
+                button.ForeColor = Color.White;
+            }
+            else if (a.Tipo.Replace(" ", "") == "Retiro")//retiro
+            {
+                button.BackColor = Color.Red;
+                button.ForeColor = Color.White;
+            }
+            button.Tag = a;
+
+            return button;
+        }
     }
 }
